4489e5b087564d7ead9dec5c159693f0f90b2dcc
amanzi-lucasvaz <lucas.vaz@amanzi.io>
2024-03-26 00:01:16 -0300

    feat: add pagination to list all patients route

--- src/app/controllers/Paciente.controller.ts
+++ src/app/controllers/Paciente.controller.ts
@@ -3,9 +3,10 @@ import PacienteService from '../services/Paciente.service';
 
 class PacienteController {
 
-    static async listAllPatients( _req: Request, res: Response ): Promise<Response> {
+    static async listAllPatients( req: Request, res: Response ): Promise<Response> {
         try {
-            const patients = await PacienteService.getAllPacientes();
+            const { page = 1, page_size: pageSize = 10 } = req.query;
+            const patients = await PacienteService.getAllPacientes(parseInt(page.toString()), parseInt(pageSize.toString()));
             return res.json(patients);
         } catch (error: any) {
             return res.status(404).json({ error: error.message });
--- src/app/services/Paciente.service.ts
+++ src/app/services/Paciente.service.ts
@@ -2,9 +2,15 @@ import { pacienteRepository } from './../repositories/repositories';
 
 class PacienteService {
 
-    static async getAllPacientes() {
-        const allPatients = await pacienteRepository.find()
-        return allPatients;
+    static async getAllPacientes(page: number, pageSize: number) {
+        const [ patients, total ] = await pacienteRepository.findAndCount({
+            take: pageSize,
+            skip: (page - 1) * pageSize
+        })
+        const totalPages = Math.ceil(total / pageSize);
+        const previous = page > 1 ? page - 1 : -1
+        const next = page !== totalPages ? page + 1 : -1;
+        return { data: patients, total: patients.length, previous, next };
     }
     
 }
